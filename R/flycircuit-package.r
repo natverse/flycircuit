b1b97b1131753a9224a94408d36a570f8346c676
Gregory Jefferis <jefferis@gmail.com>
2014-01-31 10:33:15 +0000

    document fcidtable

--- R/flycircuit-package.r
+++ R/flycircuit-package.r
@@ -3,3 +3,26 @@
 #' @name flycircuit
 #' @docType package
 NULL
+
+#' fcidtable - Data.frame of flycircuit identifiers
+#' 
+#' The flycircuit database seems to have at least 3 unique identifiers for neurons
+#' These are
+#' \itemize{
+#' \item{idid}{ An integere identifier which ranged from 1:16226 in the April
+#' 2011 release}
+#' \item{gene_name}{ A character identifier of the general form
+#' "DvGlutMARCM-F003905_seg001"}
+#' \item{Name}{ A character identifier of the general form "VGlut-F-400794"}
+#' }
+#' 
+#' Although the neuron \code{Name} is generally what is reported in publications
+#' the \code{gene_name} seems to widely used and is what connected to the 
+#' confocal image file name. The \code{_seg001} suffix in gene_names refers to 
+#' the segmentation of individiual neurons from stacks that might contain
+#' multiple labelled neurons after MARCM.
+#'
+#' @name fcidtable
+#' @family flycircuit-ids
+#' @docType data
+NULL
