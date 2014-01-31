f19715ef5e789081817f03734ba1cf25b03fe1a3
Ashley Manton <ajd.manton@googlemail.com>
2014-01-31 16:40:02 +0000

    Move .onLoad and .onAttach to zzz.R

--- R/flycircuit-package.r
+++ R/flycircuit-package.r
@@ -26,16 +26,3 @@ NULL
 #' @family flycircuit-ids
 #' @docType data
 NULL
-
-# Set local directory for data storage and remote location for data download
-.onLoad <- function(libname, pkgname) {
-  options('flycircuit.localroot' = system.file(package='flycircuit'))
-  options('flycircuit.datadir' = 'data')
-  options('flycircuit.dbdir' = 'db')
-  options('flycircuit.bigmatdir' = file.path(getOption('flycircuit.datadir'), 'bigmat'))
-  options('flycircuit.remoteloc' = 'http://flybrain.mrc-lmb.cam.ac.uk/flycircuit/')
-}
-
-.onAttach <- function(libname, pkgname) {
-  options('nat.default.neuronlist' = 'dps')
-}
---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+# Set local directory for data storage and remote location for data download
+.onLoad <- function(libname, pkgname) {
+  options('flycircuit.localroot' = system.file(package='flycircuit'))
+  options('flycircuit.datadir' = 'data')
+  options('flycircuit.dbdir' = 'db')
+  options('flycircuit.bigmatdir' = file.path(getOption('flycircuit.datadir'), 'bigmat'))
+  options('flycircuit.remoteloc' = 'http://flybrain.mrc-lmb.cam.ac.uk/flycircuit/')
+}
+
+# Set default neuronlist for plotting
+.onAttach <- function(libname, pkgname) {
+  options('nat.default.neuronlist' = 'dps')
+}
