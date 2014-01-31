fae8a387624d14e2ed571e497601e1465bbf76b2
Ashley Manton <ajd.manton@googlemail.com>
2014-01-31 16:35:00 +0000

    Change default root directory to package library path

--- R/flycircuit-package.r
+++ R/flycircuit-package.r
@@ -29,7 +29,7 @@ NULL
 
 # Set local directory for data storage and remote location for data download
 .onLoad <- function(libname, pkgname) {
-  options('flycircuit.localroot' = '~/projects/flycircuit/')
+  options('flycircuit.localroot' = system.file(package='flycircuit'))
   options('flycircuit.datadir' = 'data')
   options('flycircuit.dbdir' = 'db')
   options('flycircuit.bigmatdir' = file.path(getOption('flycircuit.datadir'), 'bigmat'))
--- inst/tests/test-onLoad.R
+++ inst/tests/test-onLoad.R
@@ -1,7 +1,7 @@
 context(".onLoad")
 
 test_that("onLoad setup is correct", {
-  expect_equal(getOption('flycircuit.localroot'), '~/projects/flycircuit/')
+  expect_equal(getOption('flycircuit.localroot'), system.file(package='flycircuit'))
   expect_equal(getOption('flycircuit.remoteloc'), 'http://flybrain.mrc-lmb.cam.ac.uk/flycircuit/')
   expect_equal(getOption('flycircuit.datadir'), 'data')
   expect_equal(getOption('flycircuit.dbdir'), 'db')
