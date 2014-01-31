2676bd3fb0ec5b280520f9564b317ff20f0ef15f
Ashley Manton <ajd.manton@googlemail.com>
2014-01-31 16:50:03 +0000

    Add tests for plotting

---
+++ inst/tests/test-plotting.R
@@ -0,0 +1,10 @@
+context("Plotting")
+
+test_that("FlyCircuit neurons can be plotted", {
+  plot3d(fc_gene_name(1))
+})
+
+test_that("hclust objects are plottable in 3d", {
+  hclustres <- hclustfc(fc_gene_name(1:20))
+  expect_true({plot3d(hclustres, h=3); TRUE})
+})
