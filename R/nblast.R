bb57cde24afac5903c63d0ff0ec7446c07e4b08d
Gregory Jefferis <jefferis@gmail.com>
2014-02-04 00:25:14 +0000

    give fc_nblast default score matrices again

--- R/nblast.R
+++ R/nblast.R
@@ -1,19 +1,25 @@
 #' Return (normalised) nblast similarity scores for FlyCircuit neurons
-#'
+#' 
 #' Precomputed scores are stored in bigmatrix form on disk. The bigmatrix object
-#' is created in the global environment once per session and then reused. When
-#' normalised is TRUE, scores are in the interval [-1, 1] or, more exactly,
-#' range(scorematrix). Higher is always better. NB scores are the mean of the
-#' normalised forward and reverse scores (as used for clustering in hclustfc)
-#' but are similarity scores (not distances).
+#' is created in the global environment once per session and then reused. When 
+#' normalised is TRUE, scores are in the interval [-1, 1] or, more exactly, 
+#' range(scorematrix). Higher is always better. NB normalised scores are the 
+#' mean of the normalised forward and reverse scores (as used for clustering in 
+#' hclustfc) but are similarity scores (not distances).
+#' @details See the package vignette for an examlple of how to download a 
+#'   precomputed score matrix.
 #' @param query,target Vectors of FlyCircuit identifiers
-#' @param scorematname The score matrix to use.
+#' @param scorematname The score matrix to use. When NULL will default to 
+#'   "abc2.normdmat" if \code{normalised=TRUE}, \code{allbyallblastcv2.5.bin} 
+#'   otherwise.
 #' @param normalised Logical indicating whether to return normalised scores.
 #' @param ... A
 #' @return Matrix of scores, columns are query neurons, rows, target.
 #' @seealso \code{\link{hclustfc},\link{fc_sub_distmat}}
 #' @export
-fc_nblast <- function(query, target, scorematname='abc2.normdmat', normalised=FALSE){
+fc_nblast <- function(query, target, scorematname=NULL, normalised=FALSE){
+  if(is.null(scorematname))
+    scorematname <- if(normalised) "abc2.normdmat" else "allbyallblastcv2.5.bin"
   scoremat <- fc_attach_bigmat(scorematname)
   available_gns <- rownames(scoremat)
   if(missing(target)) target <- rownames(scoremat)
