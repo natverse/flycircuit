9d7cd3f66b43d2443ed6dc773c244744859c79f6
Gregory Jefferis <jefferis@gmail.com>
2014-02-07 21:43:35 +0000

    remove remoteloc test and add scoremat one

--- inst/tests/test-onLoad.R
+++ inst/tests/test-onLoad.R
@@ -1,6 +1,6 @@
 context(".onLoad")
 
 test_that("onLoad setup is correct", {
-  expect_equal(getOption('flycircuit.remoteloc'), 'http://flybrain.mrc-lmb.cam.ac.uk/flycircuit')
   expect_equal(getOption('nat.default.neuronlist'), 'dps')
+  expect_equal(getOption('flycircuit.scoremat'), 'kcs20s')
 })
