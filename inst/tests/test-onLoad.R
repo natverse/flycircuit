11c48e9b8454032113e3987bd3b570e7b6a62d07
Gregory Jefferis <jefferis@gmail.com>
2014-03-03 17:52:20 +0000

    no longer test for setting of nat.default.neuronlist

--- inst/tests/test-onLoad.R
+++ inst/tests/test-onLoad.R
@@ -1,6 +1,5 @@
 context(".onLoad")
 
 test_that("onLoad setup is correct", {
-  expect_equal(getOption('nat.default.neuronlist'), 'dps')
   expect_equal(getOption('flycircuit.scoremat'), 'kcs20scores')
 })
