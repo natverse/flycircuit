bf18a70f4ac08b2e449ea3bdf646661afccd6513
James Manton <ajd.manton@googlemail.com>
2014-09-02 17:26:02 +0100

    Add as.data.frame.APResult()

--- R/clustering.R
+++ R/clustering.R
@@ -39,3 +39,34 @@ hclustfc <- function(gns, method='ward', scoremat=getOption('flycircuit.scoremat
   if(unsquare) hc$height=sqrt(hc$height)
   hc
 }
+
+
+#' Convert APResult (apcluster output) to dataframe
+#' 
+#' This can then be used for plots, clustering etc.
+#' 
+#' @param x an \code{APResult} object.
+#' @param row.names ignored.
+#' @param optional logical. If \code{TRUE}, setting row names and converting column names (to syntactic names: see make.names) is optional.
+#' @param clusters a character vector of the names of the exemplars to include.
+#' @param ... extra arguments to pass to \code{data.frame}.
+#' @return A \code{data.frame} with columns \code{exemplar}, \code{cluster},
+#'   \code{idx}, \code{item}.
+#' @export
+#' @seealso \code{\link[apcluster]{apcluster}},\code{\link[apcluster]{APResult}}
+as.data.frame.APResult <- function(x, row.names=NULL, optional=FALSE, clusters, ...) {
+  if(missing(clusters))
+    exemplars=names(x@exemplars)
+  else
+    exemplars=intersect(clusters,names(x@exemplars))
+  
+  clusterids=which(names(x@exemplars)%in%exemplars)
+  
+  clusters=x@clusters[clusterids]
+  cls=sapply(clusters,length)
+  ulc=unlist(clusters)
+  df=data.frame(exemplar=factor(rep(exemplars,cls)),
+                cluster=rep(clusterids,cls),
+                idx=ulc,item=names(ulc),row.names=names(ulc),optional=optional,stringsAsFactors=FALSE,...=...)
+  df
+}
