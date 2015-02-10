26384644c8e3f5c1bf393b54425bb903b2dcf805
Gregory Jefferis <jefferis@gmail.com>
2015-02-09 18:44:35 -0800

    Document flycircuit.scoremat in package help

--- R/flycircuit-package.r
+++ R/flycircuit-package.r
@@ -23,8 +23,15 @@
 #'   \item \code{flycircuit.bigmatdir}: location of \code{big.matrix} objects 
 #'   previously used for all by all score matrices.
 #'   
-#'   \item \code{flycircuit.scoremat}: name of the default regular or
-#'   \code{ff} matrix object containing nblast scores.
+#'   \item \code{flycircuit.scoremat}: name of the default regular or \code{ff} 
+#'   matrix object containing nblast scores.
+#'   
+#'   \item \code{flycircuit.remote_overwrite}: Determines how local cached 
+#'   versions of data available on the web are updated. When \code{NULL} (i.e. 
+#'   unset) the web version is checked and, if it is newer, it is donwloaded and
+#'   used to overwrite the cached copy. When \code{FALSE} the exisiting local
+#'   copy is used; when \code{TRUE} the remote version is always downloaded.
 #'   
 #'   }
+#'   @seealso \code{\link{fc_download_data}}
 NULL
