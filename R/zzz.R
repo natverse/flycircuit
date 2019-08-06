0dcd63fcaec9ed40f46f4ccf328c0fbfed8e057b
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-08-07 00:18:41 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -25,6 +25,36 @@
   dir.create(file.path(getOption('flycircuit.dbdir')), showWarnings=FALSE)
   dir.create(file.path(getOption('flycircuit.bigmatdir')), showWarnings=FALSE)
   dir.create(file.path(getOption('flycircuit.ffdir')), showWarnings=FALSE)
+  
+  # Note that the Morpho affine registrations need to be inverted
+  # see e.g. elmr::tpsreg for discussion of source/reference conventions
+  fcwb_chiangm <-
+    reglist(
+      solve(readRDS(system.file(
+        "extdata/InitialAffine/initialiseCMTKreg_ChiangMaleTowardsFCWB.rds",
+        package = 'flycircuit'
+      ))),
+      cmtkreg(system.file(
+        "extdata/warp/FCWB_typicalbrainmale_01_warp_m0g80c8e1e-1x26r4.list/",
+        package = 'flycircuit'
+      ))
+    )
+  
+  fcwb_chiangf <-
+    reglist(
+      solve(readRDS(system.file(
+        "extdata/InitialAffine/initialiseCMTKreg_ChiangFemaleTowardsFCWB.rds",
+        package = 'flycircuit'
+      ))),
+      cmtkreg(system.file(
+        "extdata/warp/FCWB_typicalbrainfemale_01_warp_m0g80c8e1e-1x26r4.list/",
+        package = 'catnat'
+      ))
+    )
+  
+  nat.templatebrains::add_reglist(fcwb_chiangf, reference = nat.flybrains::FCWB, sample = 'chiangf')
+  nat.templatebrains::add_reglist(fcwb_chiangm, reference = nat.flybrains::FCWB, sample = 'chiangm')
+  
 }
 
 # environment to store downloaded data to avoid polluting global namespace
