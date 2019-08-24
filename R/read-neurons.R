2f3374a7e79a5355de9de6aac4e43069be4a355c
Gregory Jefferis <jefferis@gmail.com>
2019-08-24 12:43:59 +0100

    doc / crosss-ref fixes

--- R/read-neurons.R
+++ R/read-neurons.R
@@ -1,33 +1,35 @@
 #' Fetch FlyCircuit neuron skeletons from the Taiwan FlyCircuit server
 #'
-#' @description Reads FlyCircuit skeletons from \url{http://www.flycircuit.tw/}, and bridges them into
-#' the FCWB space.
+#' @description Reads FlyCircuit skeletons from \url{http://www.flycircuit.tw/},
+#'   and bridges them into the FCWB space.
 #'
-#' @param fc.ids a vectors of valid FlyCircuit neuron ids. To acquire these in bulk, see \code{\link{flycircuit_get_ids}}.
-#' @param female whether the neurons to be transformed are female (TRUE) or male (FALSE)
-#' @param x some neuron or neuronlist
+#' @param fc.ids vector of valid FlyCircuit neuron ids. To acquire these in
+#'   bulk, see \code{\link{flycircuit_get_ids}} and
+#'   \code{\link{flycircuit-ids}}.
 #' @param ... additional arguments passed to methods
-#' 
-#' @examples 
+#' @seealso \code{\link{flycircuit_get_ids}}, \code{\link{flycircuit-ids}},
+#'   \code{\link{read.neurons}}
+#'
+#' @examples
 #' \donttest{
 #' # Let's read a neuron from the FlyCircuit database
 #' library(nat.flybrains)
 #' fcn <- flycircuit_read_neurons("Gad1-F-200234")
 #' plot3d(fcn)
 #' plot3d(FCWB)
-#' 
+#'
 #' # We can also read all neurons
 #' clear3d()
 #' fc.ids = flycircuit_get_ids()
 #' fcns <- flycircuit_read_neurons(fc.ids)
 #' plot3d(fcns)
 #' plot3d(FCWB, alpha = 0.1)
-#' 
+#'
 #' # Now mirror all neurons to the right of the brain
 #' left.somas <- function(neuron,bound = boundingbox(FCWB.surf)[1,1]+((boundingbox(FCWB.surf)[2,1]-boundingbox(FCWB.surf)[1,1])/2)){
-#'     r = nat::rootpoints(neuron)
-#'     position = nat::xyzmatrix(neuron$d[r,])
-#'     position[,"X"]>bound
+#'   r = nat::rootpoints(neuron)
+#'   position = nat::xyzmatrix(neuron$d[r,])
+#'   position[,"X"]>bound
 #' }
 #' leftsomas = unlist(nat::nlapply(fcns,left.somas))
 #' fcsleft = nat.templatebrains::mirror_brain(fcns[leftsomas], brain = FCWB)
@@ -35,7 +37,8 @@
 #' }
 #' @source \url{http://www.flycircuit.tw/}
 #' @seealso \code{\link{flycircuit_get_ids}}, \code{\link{flycircuit_page}}
-#' @return A \code{neuronlist} of FlyCircuit neurons registered in the intersex FCWB brain space
+#' @return A \code{neuronlist} of FlyCircuit neurons registered in the intersex
+#'   FCWB brain space
 #' @export
 flycircuit_read_neurons <- function(fc.ids, ...){
   ids = c()
