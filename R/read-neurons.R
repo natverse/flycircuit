b9757022eb327fb66231d5e116f40f60fbdd13ee
Gregory Jefferis <jefferis@gmail.com>
2022-08-19 18:48:26 +0100

    more efficient and less noisy xforms
    * just do one round each for male brains to avoid a message for every neuron transformed

--- R/read-neurons.R
+++ R/read-neurons.R
@@ -85,6 +85,12 @@ fc_reroot_neuron <- function(x){
 # hidden
 #' @importFrom nat.templatebrains xform_brain
 Chiang2FCWB <- function(x, female = grepl("-F-", names(x)), ...) {
-  template_to_use=ifelse(female, "chiangf","chiangm")
-  nat::nmapply(xform_brain, x, sample=template_to_use, MoreArgs = list(reference=nat.flybrains::FCWB), ...)
+  
+  if(any(female)) {
+    xf=xform_brain(x, subset=female, sample='chiangf', reference=nat.flybrains::FCWB, ...)
+  }
+  if(!all(female)) {
+    xfm=xform_brain(xf, subset=!female, sample='chiangm', reference=nat.flybrains::FCWB, ...)
+  }
+  xfm
 }
